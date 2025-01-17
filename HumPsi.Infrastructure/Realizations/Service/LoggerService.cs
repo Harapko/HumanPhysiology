1143636a5faf5eb129528c033e23405fa585c168
Harapko <harapkomaskym@gmail.com>
2025-01-17 20:17:10 +0200

    Feat/18/create logger service

---
+++ HumPsi.Infrastructure/Realizations/Service/LoggerService.cs
@@ -0,0 +1,36 @@
+using Microsoft.Extensions.Logging;
+using ILogger = Serilog.ILogger;
+
+namespace HumPsi.Infrastructure.Realizations.Service;
+using HumPsi.Application.Abstraction.IService;
+
+
+public class LoggerService(ILogger logger) : ILoggerService
+{
+    public void LogInformation(string msg)
+    {
+        logger.Information(msg);
+    }
+
+    public void LogWarning(string msg)
+    {
+        logger.Warning(msg);
+    }
+
+    public void LogTrace(string msg)
+    {
+        logger.Information(msg);
+    }
+
+    public void LogDebug(string msg)
+    {
+        logger.Debug(msg);
+    }
+
+    public void LogError(object request, string errorMsg)
+    {
+        string requestType = request.GetType().ToString();
+        string requestClass = requestType.Substring(requestType.LastIndexOf('.') + 1);
+        logger.Error($"{requestClass} handled with the error: {errorMsg}");
+    }
+}
