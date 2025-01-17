d02ca7852649a7f9ca11ce10aec2630bf591288b
Harapko <harapkomaskym@gmail.com>
2025-01-17 21:17:35 +0200

    delete obj files

--- HumPsi.Api/Controllers/SectionController.cs
+++ HumPsi.Api/Controllers/SectionController.cs
@@ -13,47 +13,47 @@ namespace HumPsi.Api.Controllers;
 [Route("[action]")]
 public class SectionController(IMediator mediator) : ControllerBase
 {
-    [HttpGet]
-    public async Task<ActionResult<List<GetSectionDtoResponse>>> GetAllSectionAsync()
-    {
-        var result = await mediator.Send(new GetAllSectionQuery());
-
-        if (result.Count == 0)
-            return NotFound("Section not found or null");
-
-        return result;
-    }
-
-    [HttpPost]
-    public async Task<ActionResult<string>> CreateSectionAsync([FromBody] CreateSectionDtoRequest request)
-    {
-        var result = await mediator.Send(new CreateSectionCommand(request));
-
-        if (result.code == 0)
-            return BadRequest(result.text);
-
-        return Ok(new {message = result.text});
-    }
-
-    [HttpPut]
-    public async Task<ActionResult<string>> UpdateSectionAsync([FromBody] UpdateSectionDtoRequest request)
-    {
-        var result = await mediator.Send(new UpdateSectionCommand(request));
-        
-        if (result.code == 0)
-            return BadRequest(result.text);
-        
-        return Ok(new { message = result.text});
-    }
-
-    [HttpDelete]
-    public async Task<ActionResult<string>> DeleteSectionAsync([FromBody] Guid id)
-    {
-        var result = await mediator.Send(new DeleteSectionCommand(id));
-        
-        if (result == Guid.Empty)
-            return BadRequest("Delete is failure");
-        
-        return Ok(new { message = $"Section {id} was deleted" });
-    }
+    // [HttpGet]
+    // public async Task<ActionResult<List<GetSectionDtoResponse>>> GetAllSectionAsync()
+    // {
+    //     var result = await mediator.Send(new GetAllSectionQuery());
+    //
+    //     if (result.Count == 0)
+    //         return NotFound("Section not found or null");
+    //
+    //     return result;
+    // }
+    //
+    // [HttpPost]
+    // public async Task<ActionResult<string>> CreateSectionAsync([FromBody] CreateSectionDtoRequest request)
+    // {
+    //     var result = await mediator.Send(new CreateSectionCommand(request));
+    //
+    //     if (result.code == 0)
+    //         return BadRequest(result.text);
+    //
+    //     return Ok(new {message = result.text});
+    // }
+    //
+    // [HttpPut]
+    // public async Task<ActionResult<string>> UpdateSectionAsync([FromBody] UpdateSectionDtoRequest request)
+    // {
+    //     var result = await mediator.Send(new UpdateSectionCommand(request));
+    //     
+    //     if (result.code == 0)
+    //         return BadRequest(result.text);
+    //     
+    //     return Ok(new { message = result.text});
+    // }
+    //
+    // [HttpDelete]
+    // public async Task<ActionResult<string>> DeleteSectionAsync([FromBody] Guid id)
+    // {
+    //     var result = await mediator.Send(new DeleteSectionCommand(id));
+    //     
+    //     if (result == Guid.Empty)
+    //         return BadRequest("Delete is failure");
+    //     
+    //     return Ok(new { message = $"Section {id} was deleted" });
+    // }
 }
